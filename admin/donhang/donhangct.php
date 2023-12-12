154604c3c8ef7a86c9b236b044aa7c6ad979f0ed
vudinhhuy2004 <huyvudinh04@gmail.com>
2023-12-13 01:50:52 +0700

    hihi

--- admin/donhang/donhangct.php
+++ admin/donhang/donhangct.php
@@ -1,23 +1,30 @@
 <div class="main-content">
     <h3 class="title-page" style="color: red;text-align: center;margin: 40px">Đơn hàng chi tiết</h3>
+    <form action="index.php?act=donhangct" method="post">
     <table id="example" class="table table-striped" style="width: 100%">
         <thead>
-        <tr>
-            <th>ID sản phẩm</th>
-            <th>Tên</th>
-            <th>Hình ảnh</th>
-            <th>Giá</th>
-            <th>Số lượng</th>
-            <th>Thao tác</th>
-        </tr>
+            <tr>
+                <th>ID đơn hàng</th>
+                <th>ID sản phẩm</th>
+                <th>Tên</th>
+                <th>Hình ảnh</th>
+                <th>Giá</th>
+                <th>Số lượng</th>
+                <th>Thao tác</th>
+            </tr>
         </thead>
         <tbody>
         <?php
-                   extract($dhct);
-                   global $hinh;
-                   $hinh = $hinh_path.$img;
+                   extract($donhang);
+                    $hinh="../img/".$img;
+                    if (is_file($hinh)) {
+                        $img="<img src='".$hinh."' height='80'>";
+                    }else{
+                        $hinh="no photo";
+                    }
                    echo '<tr>
-                        <td>'.$id.'</td>
+                        <td>'.$iddh.'</td>
+                        <td>'.$idsp.'</td>
                         <td>' . $name . '</td>
                         <td>' . $hinh . '</td>
                         <td>' . $price . '</td>
@@ -30,6 +37,7 @@
         ?>
         </tbody>
     </table>
+    </form>
 </div>
 </div>
 </div>
